8c28ca68eeed4444410d16818212bc61fa5f42b0
Diego Oliveira <diego@thdr.com.br>
2024-09-28 11:01:14 -0300

    Installing React project using Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react' // pacote responsável pelo core do React
+import { createRoot } from 'react-dom/client'  // integração do react (Core do React) com a DOM (Document Objetc Model - Representação do HMTL através JavaScript)
+import { App } from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
+
+createRoot(document.getElementById('root')).render(  //Método createRoot recebe o elemento raiz da página html e cria o HTML/CSS/JS dentro desta DIV
+  <StrictMode> 
+    <App />    
   </StrictMode>,
 )
