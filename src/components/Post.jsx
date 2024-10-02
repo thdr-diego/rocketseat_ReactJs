88431b25f65d8e796787b28b19b1461a51172a5e
Diego Oliveira <diego@thdr.com.br>
2024-10-01 22:20:31 -0300

    adding react components and css modules

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import styles from './Post.module.css';
+import { Comment } from './Comment.jsx';
 
 export function Post() {
     return (
@@ -23,6 +24,23 @@ export function Post() {
                 <a href="#">#nlw{' '}</a>
                 <a href="#">#rocketseat{' '}</a></p>
             </div>
+
+            <form className={styles.commentForm}>
+            <strong>Deixe seu Feedback</strong>
+
+            <textarea
+            placeholder="Deixe um comentário"          
+            />
+            <footer>
+            <button type="submit">Publicar</button>
+            </footer>
+            </form>
+
+           <div className={styles.commentList}>
+           < Comment/>
+           < Comment/>
+           < Comment/>
+           </div>
         </article>
     )
 }
